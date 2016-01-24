2d14d709729e4c685274758738d2a193c2ed70e5
Adrian <adrian.bozhankov@gmail.com>
2016-01-24 15:31:40 +0200

    Added repository pattern

---
+++ HowDeepIsYourLove/Goldddigger.Data/Contracts/IGolddiggerDbContext.cs
@@ -0,0 +1,21 @@
+﻿namespace Golddigger.Data.Contracts
+{
+    using System.Data.Entity;
+    using System.Data.Entity.Infrastructure;
+    using Golddigger.Models;
+
+    public interface IGolddiggerDbContext
+    {
+        //IDbSet<Comment> Comments { get; set; }
+
+        //IDbSet<RealEstate> RealEstates { get; set; }
+
+        DbSet<TEntity> Set<TEntity>() where TEntity : class;
+
+        DbEntityEntry<TEntity> Entry<TEntity>(TEntity entity) where TEntity : class;
+
+        void Dispose();
+
+        int SaveChanges();
+    }
+}
