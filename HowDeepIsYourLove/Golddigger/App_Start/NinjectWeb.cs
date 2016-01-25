3a24dd5fe5591cadbe4939258c2af46f8eff5e79
Alex-Bubblemaster <shiatzu@gmail.com>
2016-01-25 10:16:05 +0200

    Addded NinjectWeb and NinjectWebCommon

---
+++ HowDeepIsYourLove/Golddigger/App_Start/NinjectWeb.cs
@@ -0,0 +1,17 @@
+﻿[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(Golddigger.App_Start.NinjectWeb), "Start")]
+
+namespace Golddigger.App_Start
+{
+    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+    using Ninject.Web.Common;
+    public static class NinjectWeb
+    {
+        /// <summary>
+        /// Starts the application
+        /// </summary>
+        public static void Start()
+        {
+            DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
+        }
+    }
+}
